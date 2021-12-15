98def1f0648805ac496da46072d9c488428c3e21
swifty.wang <swifty.wang@binance.com>
2021-12-15 09:32:42 +0800

    update target api 30

--- src/main/java/Configs.kt
+++ src/main/java/Configs.kt
@@ -2,7 +2,7 @@
  * Created by Swifty Wang on 4/7/2020.
  */
 object Configs {
-  const val COMPILE_SDK = 29
-  const val TARGET_SDK = 29
+  const val COMPILE_SDK = 30
+  const val TARGET_SDK = 30
   const val MIN_SDK = 16
 }
