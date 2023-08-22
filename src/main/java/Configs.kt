d84b430ccd4f5dabb08e2d238c3bcfd9c7958e05
swifty.wang <swifty.wang@binance.com>
2023-08-22 11:05:06 +0800

    bump sdk to 34

--- src/main/java/Configs.kt
+++ src/main/java/Configs.kt
@@ -2,7 +2,7 @@
  * Created by Swifty Wang on 4/7/2020.
  */
 object Configs {
-  const val COMPILE_SDK = 31
-  const val TARGET_SDK = 31
+  const val COMPILE_SDK = 34
+  const val TARGET_SDK = 34
   const val MIN_SDK = 19
 }
