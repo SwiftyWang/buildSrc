6d261b8ec0a480b868d24e16ab2dcff349598b2a
swifty.wang <swifty.wang@binance.com>
2024-11-04 12:18:07 +0800

    upgrade android/plugin/gradle version and use latest data source

--- src/main/java/Configs.kt
+++ src/main/java/Configs.kt
@@ -2,7 +2,7 @@
  * Created by Swifty Wang on 4/7/2020.
  */
 object Configs {
-  const val COMPILE_SDK = 34
-  const val TARGET_SDK = 34
+  const val COMPILE_SDK = 35
+  const val TARGET_SDK = 35
   const val MIN_SDK = 21
 }
--- src/main/java/Versions.kt
+++ src/main/java/Versions.kt
@@ -2,11 +2,11 @@ import org.gradle.api.JavaVersion
 
 object Versions {
 
-    const val ANDROID = "7.2.2"
+    const val ANDROID = "8.4.2"
     const val GMS = "4.3.5"
-    const val KOTLIN = "1.7.21"
+    const val KOTLIN = "1.9.25"
 
-    const val OBJECT_BOX = "3.1.3" // 3.2.1+ need kotlin 1.7
+    const val OBJECT_BOX = "4.0.3" // 3.2.1+ need kotlin 1.7
 
     const val BENCHMARK = "1.0.0"
     const val GOOGLE_SERVICES = "4.3.3"
@@ -14,21 +14,21 @@ object Versions {
     const val NAVIGATION = "2.2.2"
     const val HILT = "2.33-beta"
 
-    val COMPAT_JAVA_VERSION = JavaVersion.VERSION_11
+    val COMPAT_JAVA_VERSION = JavaVersion.VERSION_21
 
-    const val ANDROIDX_APPCOMPAT = "1.4.2"
-    const val ANDROIDX_BROWSER = "1.2.0"
+    const val ANDROIDX_APPCOMPAT = "1.7.0"
+    const val ANDROIDX_BROWSER = "1.8.0"
     const val ANDROIDX_SWIPE_REFRESH = "1.1.0"
-    const val ANDROIDX_MATERIAL = "1.6.1"
-    const val ANDROIDX_RECYCLERVIEW = "1.1.0"
-    const val ANDROIDX_CONSTRAINT = "2.1.3"
+    const val ANDROIDX_MATERIAL = "1.12.0"
+    const val ANDROIDX_RECYCLERVIEW = "1.3.2"
+    const val ANDROIDX_CONSTRAINT = "2.2.0"
     const val ANDROIDX_CARD_VIEW = "1.0.0"
     const val ANDROIDX_MULTIDEX = "2.0.1"
     const val ANDROIDX_KOTLIN = "1.3.2"
     const val ANDROIDX_LIFECYCLE = "2.5.1"
-    const val ANDROIDX_JUNIT = "1.1.3"
+    const val ANDROIDX_JUNIT = "1.2.1"
     const val ANDROIDX_RULES = "1.3.0"
-    const val ANDROIDX_ESPRESSO = "3.4.0"
+    const val ANDROIDX_ESPRESSO = "3.6.1"
     const val ANDROIDX_HILT = "1.0.0-alpha03"
     const val ANDROIDX_KTX_FRAGMENT = "1.3.0"
     const val ANDROIDX_KTX_RX_TO_LIVEDATA = "2.2.0"
@@ -47,12 +47,12 @@ object Versions {
     const val RX_JAVA3 = "3.0.13"
     const val RX_ANDROID3 = "3.0.0"
 
-    const val FIREBASE_ADS = "23.0.0"
+    const val FIREBASE_ADS = "23.5.0"
     const val FIREBASE_BOM = "32.1.0"
 
-    const val GSON = "2.9.0"
+    const val GSON = "2.10.1"
     const val JUNIT = "4.13.2"
-    const val DAGGER = "2.38.1"
+    const val DAGGER = "2.50"
     const val RETROFIT = "2.9.0"
     const val ROBOLECTRIC = "3.8"
     const val GLIDE = "4.11.0"
